7b4f712e060a07c92cd11b01fe6c4ba679273ed6
AnhTuvx <zzkuxinzz@gmail.com>
2024-10-02 20:06:18 +0700

    finish intro page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sushi_restaurant_flutter/compoments/button.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 150, 55, 48),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            //shop name
+            Text(
+              "SUSHI APP",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 30, // Specify font size in pixels
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: EdgeInsets.all(50.0),
+              child: Image.asset("lib/images/kappa-maki_5053383.png"),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Text(
+              "THE TASTE OF JAPANESE FOOD",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 44,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Feel the taste of the most popular Japanese food from anywhere and anytime",
+              style: TextStyle(
+                color: Colors.grey[300],
+                height: 2,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            MyButton(
+              text: "Get Started",
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
